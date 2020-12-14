34cd4399916c64f7ed5be628c285de7695834965
etillison3350 <ethan.l.tillison@gmail.com>
2020-12-14 00:22:47 -0500

    Day 14

--- src/day14.py
+++ src/day14.py
@@ -4,9 +4,45 @@ import input
 from re import *
 
 
+def get_addrs(mask: str, val: int):
+    if 'X' not in mask:
+        return [int(mask, 2) | val]
+    else:
+        fi = mask.index('X')
+        return get_addrs(mask[:fi] + '0' + mask[fi+1:], val) + get_addrs(mask[:fi] + '1' + mask[fi+1:], val)
+
+
 def run(r: Sequence[str]):
-    print(r)
-    print(len(r))
+    mem = {}
+
+    mask = 'X' * 36
+    clr_mask = set_mask = 0
+    for line in r:
+        if line[1] == 'a':
+            mask = line[7:]
+            set_mask = int(mask.replace('X', '0'), 2)
+            clr_mask = int(mask.replace('X', '1'), 2)
+        else:
+            cmd = search('\\[(\\d+)] = (\\d+)', line)
+            addr = int(cmd.group(1))
+            val = int(cmd.group(2))
+            mem[addr] = (val & clr_mask) | set_mask
+    print(sum([v for k, v in mem.items()]))
+
+    mem = {}
+    for line in r:
+        if line[1] == 'a':
+            mask = line[7:]
+            set_mask = int(mask.replace('0', '1').replace('X', '0'), 2)
+        else:
+            cmd = search('\\[(\\d+)] = (\\d+)', line)
+            addr = int(cmd.group(1)) & set_mask
+            val = int(cmd.group(2))
+            for a in get_addrs(mask, addr):
+                mem[a] = val
+    if len(mem) < 100:
+        print(mem)
+    print(sum([v for k, v in mem.items()]))
 
 
 if __name__ == '__main__':
@@ -23,7 +59,7 @@ if __name__ == '__main__':
     print()
 
     print('Possible test cases:')
-    test_cases = input.find_test_cases(day, year, cached=False)
+    test_cases = input.find_test_cases(day, year, cached=True)
     for index, tc in enumerate(test_cases):
         tc_list = tc.split(split_seq)
         print('Test case {}: {}'.format(index, str(tc_list)[:80]))

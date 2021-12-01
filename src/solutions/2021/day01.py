62decef8a8f6d3b574246d9a7c5a6919a9439cc8
Ethan Tillison <ethan.l.tillison@gmail.com>
2021-12-01 00:24:20 -0500

    Day 1 Original Code

--- src/solutions/2021/day01.py
+++ src/solutions/2021/day01.py
@@ -3,6 +3,7 @@ from typing import Any, Callable, Generator, Sequence
 from helpers.executor import Executor
 
 import itertools
+from itertools import *
 import re
 from re import *
 
@@ -16,12 +17,27 @@ class Solution(Executor):
         yield self._solve_part2(r, print)
 
     def _solve_part1(self, r: Sequence[str], print: Callable[..., None]) -> Any:
-        # TODO
-        return None
+        k = [int(v) for v in r]
+        ans = 0
+        for i in range(1, len(k)):
+            if k[i] > k[i - 1]:
+                ans += 1
+        return ans
 
     def _solve_part2(self, r: Sequence[str], print: Callable[..., None]) -> Any:
-        # TODO
-        return None
+        k = [int(v) for v in r]
+        ans = 0
+
+        s = []
+
+        for i in range(3, len(k) + 1):
+            s.append(sum(k[i - 3:i]))
+
+        for i in range(1, len(s)):
+            if s[i] > s[i - 1]:
+                ans += 1
+
+        return ans
 
 
 if __name__ == '__main__':
